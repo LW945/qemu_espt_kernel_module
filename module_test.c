7a6f63fea7c5b201b2cebafc7cab693390a708a7
LW945 <lw945lw945@yahoo.com>
2020-10-15 05:38:24 -0700

    v0.2

--- module_test.c
+++ module_test.c
@@ -10,26 +10,47 @@
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("netlink example");
 
-#define ESPT_SET_ENTRY 0
-#define ESPT_FLUSH_ENTRY 1
+#define ESPT_INIT 0
+#define ESPT_SET_ENTRY 1
+#define ESPT_FLUSH_ENTRY 2
+#define ESPT_MMIO_ENTRY 3
 
 typedef pgd_t* (*custom_pgd_alloc)(struct mm_struct *mm);
 typedef int (*custom_pud_alloc)(struct mm_struct *mm, p4d_t *p4d, unsigned long address);
 typedef int (*custom_pmd_alloc)(struct mm_struct *mm, pud_t *pud, unsigned long address);
 typedef int (*custom_pte_alloc)(struct mm_struct *mm, pmd_t *pmd);
 
+custom_pgd_alloc my_pgd_alloc; 
+custom_pud_alloc my_pud_alloc; 
+custom_pmd_alloc my_pmd_alloc;
+custom_pte_alloc my_pte_alloc;
+
+pgd_t * gva_pgd, * hva_pgd;
+pud_t * gva_pud, * hva_pud;
+pmd_t * gva_pmd, * hva_pmd;
+pte_t * gva_pte, * hva_pte;
+
+struct task_struct *task;
+struct mm_struct *mm;
+uint64_t gva, hva, pa_gva, pa_hva;
+
+int pid;
+
 struct ESPTEntry{
 	union{
 		struct{
 			uint64_t gva;
 			uintptr_t hva;
-			int pid;
 		}set_entry;
 		struct{
 			uint64_t *list;
 			int size;
-			int pid;
 		}flush_entry;
+		struct{
+			uint64_t gva;
+			uint64_t val;
+			int add;
+		}mmio_entry;
 	};
 };
 
@@ -53,30 +74,8 @@ static int espt_dev_mmap(struct file *file, struct vm_area_struct *vma)
 
 static int espt_dev_ioctl_set_entry(struct ESPTEntry espt_entry)
 {
-	struct task_struct *task;
-	struct mm_struct *mm;
-	uint64_t pa_gva, pa_hva;
-	uint64_t gva = espt_entry.set_entry.gva;
-	uint64_t hva = espt_entry.set_entry.hva;
-	int pid = espt_entry.set_entry.pid;
-	
-	pgd_t * gva_pgd, * hva_pgd;
-	pud_t * gva_pud, * hva_pud;
-	pmd_t * gva_pmd, * hva_pmd;
-	pte_t * gva_pte, * hva_pte;
-
-	custom_pgd_alloc my_pgd_alloc;
-	custom_pud_alloc my_pud_alloc;
-	custom_pmd_alloc my_pmd_alloc;
-	custom_pte_alloc my_pte_alloc;
-
-	my_pgd_alloc = (custom_pgd_alloc)kallsyms_lookup_name("pgd_alloc");
-	my_pud_alloc = (custom_pud_alloc)kallsyms_lookup_name("__pud_alloc");
-	my_pmd_alloc = (custom_pmd_alloc)kallsyms_lookup_name("__pmd_alloc");
-	my_pte_alloc = (custom_pte_alloc)kallsyms_lookup_name("__pte_alloc");
-
-	task = get_pid_task(find_get_pid(pid),PIDTYPE_PID);
-	mm = task->mm;
+	gva = espt_entry.set_entry.gva;
+	hva = espt_entry.set_entry.hva;
 
 	hva_pgd = pgd_offset(mm, hva);
 	if(!my_pud_alloc(mm, &__p4d(pgd_val(*hva_pgd)), hva)){		
@@ -106,9 +105,7 @@ static int espt_dev_ioctl_set_entry(struct ESPTEntry espt_entry)
 				gva_pte = pte_offset_kernel(gva_pmd, gva);
 				pa_gva = (pte_pfn(*gva_pte) << PAGE_SHIFT) | (gva & ~PAGE_MASK);
 				pa_hva = (pte_pfn(*hva_pte) << PAGE_SHIFT) | (hva & ~PAGE_MASK);	
-				if(pa_gva != pa_hva){
-					set_pte(gva_pte, *hva_pte);
-				}
+				set_pte(gva_pte, *hva_pte);
 			}			
 		}
 	}
@@ -117,32 +114,10 @@ static int espt_dev_ioctl_set_entry(struct ESPTEntry espt_entry)
 
 static int espt_dev_ioctl_flush_entry(struct ESPTEntry espt_entry)
 {
-	struct task_struct *task;
-	struct mm_struct *mm;
-	uint64_t gva;
 	int i;
-
 	int len = espt_entry.flush_entry.size;
-	int pid = espt_entry.flush_entry.size;
 	uint64_t *list = espt_entry.flush_entry.list;
 
-	pgd_t * gva_pgd;
-	pud_t * gva_pud;
-	pmd_t * gva_pmd;
-	pte_t * gva_pte;
-
-	custom_pgd_alloc my_pgd_alloc;
-	custom_pud_alloc my_pud_alloc;
-	custom_pmd_alloc my_pmd_alloc;
-	custom_pte_alloc my_pte_alloc;
-
-	my_pgd_alloc = (custom_pgd_alloc)kallsyms_lookup_name("pgd_alloc");
-	my_pud_alloc = (custom_pud_alloc)kallsyms_lookup_name("__pud_alloc");
-	my_pmd_alloc = (custom_pmd_alloc)kallsyms_lookup_name("__pmd_alloc");
-	my_pte_alloc = (custom_pte_alloc)kallsyms_lookup_name("__pte_alloc");
-
-	task = get_pid_task(find_get_pid(pid),PIDTYPE_PID);
-	mm = task->mm;
 	for(i = 0; i < len; i++){
 		gva = list[i];
 		gva_pgd = pgd_offset(mm, gva);
@@ -159,6 +134,37 @@ static int espt_dev_ioctl_flush_entry(struct ESPTEntry espt_entry)
 					set_pte(gva_pte, __pte(0));
 				}			
 			}
+		}
+	}
+	return 0;
+}
+
+static int espt_dev_ioctl_mmio_entry(struct ESPTEntry espt_entry)
+{
+	int add = espt_entry.mmio_entry.add;
+	uint64_t value = espt_entry.mmio_entry.val;
+	gva = espt_entry.mmio_entry.gva;
+
+	gva_pgd = pgd_offset(mm, gva);
+	if(!my_pud_alloc(mm, &__p4d(pgd_val(*gva_pgd)), gva)){
+		if(!(pgd_flags(*(gva_pgd)) & _PAGE_PRESENT)){
+			pgd_t * tmp = (pgd_t *)get_zeroed_page(GFP_KERNEL_ACCOUNT);
+			set_pgd(gva_pgd, __pgd(_PAGE_TABLE | __pa(tmp)));
+		}
+		gva_pud = pud_offset(&__p4d(pgd_val(*gva_pgd)), gva);
+		if(!my_pmd_alloc(mm, gva_pud, gva));{
+			gva_pmd = pmd_offset(gva_pud, gva);
+			if(!my_pte_alloc(mm, gva_pmd)){
+				gva_pte = pte_offset_kernel(gva_pmd, gva);
+				if(add){
+					pte_t * tmp = (pte_t *)get_zeroed_page(GFP_KERNEL_ACCOUNT);
+					set_pte(gva_pte, __pte(_PAGE_TABLE | __pa(tmp)));
+					*(uint64_t *)tmp = value;				
+				}
+				else{
+					set_pte(gva_pte, __pte(0));			
+				}
+			}			
 		}
 	}
 	return 0;
@@ -193,6 +199,22 @@ static long espt_dev_ioctl(struct file *filp,
 				goto out;
 			break;
 	}
+	case ESPT_INIT:{
+		int pid_value;	
+		r = -EFAULT;
+		if (copy_from_user(&pid_value, (void *)arg, sizeof(int)))
+			goto out;
+		pid = pid_value;
+		task = get_pid_task(find_get_pid(pid),PIDTYPE_PID);
+		mm = task->mm;
+	}
+	case ESPT_MMIO_ENTRY:{
+		struct ESPTEntry espt_mmio_entry;
+		r = -EFAULT;
+		if (copy_from_user(&espt_mmio_entry, (void *)arg, sizeof(int)))
+			goto out;
+		r = espt_dev_ioctl_mmio_entry(espt_mmio_entry);
+	}
 	default:
 		;
 	}
@@ -221,6 +243,12 @@ int espt_init(void)
 	if (r) {
 		printk (KERN_ERR "espt: misc device register failed\n");
 	}    
+
+	my_pgd_alloc = (custom_pgd_alloc)kallsyms_lookup_name("pgd_alloc");
+	my_pud_alloc = (custom_pud_alloc)kallsyms_lookup_name("__pud_alloc");
+	my_pmd_alloc = (custom_pmd_alloc)kallsyms_lookup_name("__pmd_alloc");
+	my_pte_alloc = (custom_pte_alloc)kallsyms_lookup_name("__pte_alloc");
+
 	printk("espt_init ok\n");
     return 0;
 }
